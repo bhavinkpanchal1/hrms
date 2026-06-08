b26277fa576ee5729a2aebc728cdc1202fbf7885
-g <-g>
2026-06-08 14:52:06 +0530

    feat(recruitment): integrate react-query for interview workflow

--- frontendts/src/modules/recruitment/candidate/hooks/useScheduleInterview.ts
+++ frontendts/src/modules/recruitment/candidate/hooks/useScheduleInterview.ts
@@ -1,9 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 import scheduleInterview from "../../services/interview.service";
 
-
 export function useScheduleInterview() {
+  const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: scheduleInterview
-  })
+    mutationFn: scheduleInterview,
+
+    onSuccess: async () => {
+      console.log("Mutation Success");
+
+      await queryClient.refetchQueries({
+        queryKey: ["interviews"],
+      });
+    },
+  });
 }
--- frontendts/src/modules/recruitment/candidate/services/getInterviews.service.ts
+++ frontendts/src/modules/recruitment/candidate/services/getInterviews.service.ts
@@ -3,5 +3,8 @@ import { interviewTableData } from "../../constants/interviewTableData";
 
 export async function getInterview() {
     await new Promise((r)=>setTimeout(r, 1000));
+    console.log(
+ "Fetching Interviews"
+);
     return interviewTableData;
 }
